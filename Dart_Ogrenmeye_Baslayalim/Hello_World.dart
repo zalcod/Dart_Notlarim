47476e8ccf8d2020186c36de038ec1e17fcf6a87
Zal Solmuş <zalsolmus@gmail.com>
2022-07-17 15:27:21 +0300

    Degisim

---
+++ Dart_Ogrenmeye_Baslayalim/Hello_World.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  print("hello world");
+  print("zal");
+  print("zal");
+  print(3 + 5);
+  print("zal'ın dersleri");
+  print("zal" + "solmuş");
+}
+
+//Çıktı
+/*
+hello world
+zal
+zal
+8
+zal'ın dersleri
+zal solmuş
+*/
